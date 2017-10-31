8803e99d35ac4c8ee6c148bff814b1acd9bc78cd
Adam Mackler <AdamMackler@gmail.com>
2017-10-31 02:25:18 -0400

    Add direct objects

--- nlg.scala
+++ nlg.scala
@@ -1,9 +1,26 @@
-// Pronouns
+// Contant enumerations
 
 object Gender extends Enumeration {
   type Gender = Value
   val Male, Female, Neuter = Value
 }
+import Gender._
+
+// Number
+object VerbNumber extends Enumeration {
+  type VerbNumber = Value
+  val Singular, Plural = Value
+}
+import VerbNumber._
+
+// Person
+object VerbPerson extends Enumeration {
+  type VerbPerson = Value
+  val First, Second, Third  = Value
+}
+import VerbPerson._
+
+// Pronouns
 
 val Pronoun: Array[Array[Array[Option[String]]]] = Array(
   // dimensions are gender: number, person
@@ -16,33 +33,57 @@ val Pronoun: Array[Array[Array[Option[String]]]] = Array(
     Array(Some("my"), Some("vy"), Some("ony")) // plur.
   ),
   Array(// stredného rodu
+    Array(None, None, Some("ono")), // sing.
+    Array(None, None, None) // plur.
+  )
+)
+
+val Demonstrative: Array[Array[Array[Option[String]]]] = Array(
+  // dimensions are gender: number, person
+  Array(                          // muž.
+    Array(None, None, Some("ten")),  // sing.
+    Array(None, None, None) // plur.
+  ),
+  Array (                         // žen.
+    Array(None, None, Some("tá")),  // sing.
+    Array(None, None, None) // plur.
+  ),
+  Array(// stredného rodu
     Array(None, None, Some("to")), // sing.
     Array(None, None, None) // plur.
   )
 )
 
+// Nouns
+
+trait Noun {
+  val gender: Gender
+  // these are just Options until I learn all the declensions
+  val nominative: Option[String]
+  val accusative: Option[String]
+}
+
+object kufor extends Noun {
+  override val gender = Male
+  override val nominative = Some("kufor")
+  override val accusative = Some("kufor")
+}
+
+object auto extends Noun {
+  override val gender = Male
+  override val nominative = None
+  override val accusative = Some("auto")
+}
+
+val nouns = Set(kufor, auto)
+
 // Verbs
 
-// Number
-object VerbNumber extends Enumeration {
-  type VerbNumber = Value
-  val Singular, Plural = Value
-}
-
-// Person
-object VerbPerson extends Enumeration {
-  type VerbPerson = Value
-  val First, Second, Third  = Value
-}
-
-import Gender._
-import VerbNumber._
-import VerbPerson._
-
 trait Verb {
-  val konjugácia: Array[Array[String]]
-
   val infinitive: String
+  val konjugácia: Array[Array[String]]
+  val isTransitive: Boolean = false
+  val isCopulative: Boolean = false
 
   def konjuguj: Array[String] = (for {
     number <- VerbNumber.values
@@ -53,6 +94,7 @@ trait Verb {
 }
 
 object Byť extends Verb {
+  override val isCopulative = true
   override val infinitive = "byť"
   override val konjugácia = Array(
     Array("som", "si", "je"),      // singular
@@ -79,26 +121,34 @@ trait chytáť extends RegularVerb {
 
 object Mať extends chytáť {
   override val root = "m"
+  override val isTransitive = true
 }
 
-val verbs = Array(Byť, Mať)
+val verbs = Set(Byť, Mať)
 
 object Main extends App {
+  // conjugate all the verbs
   verbs foreach { verb =>
-    for {
+    /*    for {
       number <- VerbNumber.values
       person <- VerbPerson.values
     } {
       println(verb.inflect(number, person))
-    }
+    }*/
 
+    // conjugate all the verbs including pronouns
     for {
       gender <- Gender.values
       number <- VerbNumber.values
       person <- VerbPerson.values
       pronoun <- Pronoun(gender.id)(number.id)(person.id)
     } {
-      println(pronoun + " " + verb.inflect(number, person))
+      // add a direct object
+      if (verb.isTransitive) nouns.map(_.accusative).filter(_.isDefined).map(_.get).foreach { directObject: String =>
+        println(pronoun + " " + verb.inflect(number, person) + " " + directObject)
+      } else // no direct object
+          println(pronoun + " " + verb.inflect(number, person))
+
     }
 
   }
